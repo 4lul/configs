ed9a326ae2618b3dc963b11481bdd04a59151ba3
4lul <91326095+4lul@users.noreply.github.com>
2022-07-07 12:12:12 +0300

    Update vim_pathogen_jedi_airline.sh

--- vim/vim_pathogen_jedi_airline.sh
+++ vim/vim_pathogen_jedi_airline.sh
@@ -2,4 +2,3 @@ cp .vimrc ~/.vimrc && \
 
 mkdir -p ~/.vim/ && cp -r autoload colors bundle ~/.vim/ && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
