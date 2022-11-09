098841a6e98dbe6af3a66b6f7973e234cd3e3026
pi <4lusal@gmail.com>
2022-11-09 16:11:05 +0300

    add feature for vim, rpi

--- vim/vim_pathogen-bundle.sh
+++ vim/vim_pathogen-bundle.sh
@@ -1,4 +1,5 @@
 cp .vimrc ~/.vimrc && \
+sudo cp .vimrc-r /root/.vimrc && \
 
 mkdir -p ~/.vim/ && cp -r autoload colors bundle ~/.vim/ && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
